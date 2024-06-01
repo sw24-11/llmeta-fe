9ff74ff2efc5c53e0627b029c33d03429b704582
kimseokhee <cat1181123@naver.com>
2024-06-01 16:44:24 +0900

    Fix: 최소 height layout 수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <Component {...pageProps} />
+    </div>
+  );
 }
