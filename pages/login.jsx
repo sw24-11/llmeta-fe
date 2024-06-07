b0a17de6b130f1d6dedd2c0aadaa745444904e7a
kimseokhee <cat1181123@naver.com>
2024-06-08 04:54:28 +0900

    Feat: 로그인 기능

--- pages/login.jsx
+++ pages/login.jsx
@@ -10,9 +10,49 @@ import {
 import { Label } from "@/components/Label";
 import { Separator } from "@/components/Separator";
 import { Input } from "@/components/Input";
-import Header from "@/components/Header.js";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-export default function login() {
+// 📌 기능 요구사항
+// 로그인 안 된 경우, alert로 로그인 하라고 띄우기
+// 로그인 후 home에는 우측 상단에 sign in 버튼 대신 sign out 버튼
+// 로그인 api 연결
+// 로그아웃 기능
+// login 버튼 상단에 sign up 버튼(텍스트 클릭 시 sign up page로 이동) 만들기
+
+// 📌 추가 요구사항
+// 로그인을 하면 session Id가 cookie로 옵니다. 정상적으로 오면 로그인 성공
+
+export default function Login() {
+  const router = useRouter();
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setLoginForm((prevForm) => ({
+      ...prevForm,
+      [id]: value,
+    }));
+  };
+
+  const handleLogin = async () => {
+    // try {
+    //   const response = await axios.post("/login", loginForm);
+    //   // 로그인 성공 시 처리 로직 추가
+    //   console.log(response.data);
+    // } catch (error) {
+    //   // 로그인 실패 시 처리 로직 추가
+    //   console.error(error);
+    // }
+
+    // localStorage에 userName 저장
+    localStorage.setItem("userName", "김석희");
+    router.push("/");
+  };
+
   return (
     <>
       <Separator className="my-4" />
@@ -30,17 +70,26 @@ export default function login() {
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
-                  placeholder="example@email.com"
+                  placeholder="cat1181123@naver.com"
                   type="email"
+                  value={loginForm.email}
+                  onChange={handleInputChange}
                 />
               </div>
               <div className="space-y-2">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
+                <Input
+                  id="password"
+                  type="password"
+                  value={loginForm.password}
+                  onChange={handleInputChange}
+                />
               </div>
             </CardContent>
             <CardFooter>
-              <Button className="w-full">Login</Button>
+              <Button className="w-full" onClick={handleLogin}>
+                Login
+              </Button>
             </CardFooter>
           </Card>
         </div>
