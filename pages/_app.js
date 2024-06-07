d07a0888a95fe4b4f695332bd0f701015a77306b
kimseokhee <cat1181123@naver.com>
2024-06-08 04:58:23 +0900

    Feat: localhost에서 userName이 없으면 로그인으로 이동

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  // login, 기본 도메인을 제외하고 localStorage에서 userName 없으면 로그인 페이지로 이동
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const userName = localStorage.getItem("userName");
+      if (
+        !userName &&
+        router.pathname !== "/login" &&
+        router.pathname !== "/"
+      ) {
+        alert("로그인이 필요합니다.");
+        router.push("/login");
+      }
+    }
+  }, [router.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Header></Header>
